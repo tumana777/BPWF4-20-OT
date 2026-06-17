7052af968ebb1bedf977625ad715f22d7dbcfb79
tumana777 <tumana991@gmail.com>
2026-06-17 21:25:03 +0400

    add lesson1 and lesson 2

---
+++ Lesson 1/main.py
@@ -0,0 +1,89 @@
+# name = 'Otar'
+# lecturer = True
+# student = False
+# age = 35
+# price = 100.50
+
+# print("Hello world!")
+
+# print(age)
+
+# print("Hello world!")
+
+# print(name, lecturer, student, age, price, sep="#")
+
+# print(name, end="!")
+# print(lecturer)
+
+# print(name)
+# print(lecturer)
+# print(student)
+# print(price)
+# print(age)
+
+
+# name = 'Otar'
+# lecturer = True
+# student = False
+# age = 35
+# price = 100.50
+
+# print(id(name))
+# print(id(lecturer))
+# print(id(student))
+# print(id(age))
+# print(id(price))
+
+# print(type(name))
+# print(type(lecturer))
+# print(type(student))
+# print(type(age))
+# print(type(price))
+
+# lecturer = False
+#
+# print(lecturer)
+
+# first_name = 'Otar'
+# last_name = 'Tumanishvili'
+
+# print("Program started...")
+#
+# name = input("Please enter your name: ")
+#
+# print("Hello world!")
+# print("My name is", name)
+
+# print("Program started...")
+#
+# number1 = 6
+# number2 = 2
+
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+# print(number1 / number2)
+# print(number1 // number2)
+# print(number1 % number2)
+# print(number1 ** number2)
+
+
+# first_name = 'Otar'
+# last_name = 'Tumanishvili'
+#
+# print(first_name + last_name)
+
+# print("Program started...")
+#
+# number1 = int(input("Please enter a number1: "))
+# number2 = int(input("Please enter a number2: "))
+#
+# print("Hello world!")
+#
+# print(number1 + number2)
+
+# d1 = '3.5'
+# d2 = 2.5
+#
+# print(type(d1))
+
---
+++ Lesson 2/main.py
@@ -0,0 +1,187 @@
+
+# a = 5
+
+# a = a + 8
+# a += 8
+
+# a = a - 2
+# a -= 2
+#
+# a = a * 2
+# a *= 2
+#
+# a = a / 2
+# a /= 2
+#
+# a = a ** 2
+# a **= 2
+#
+# a = a % 2
+# a %= 2
+#
+# a = a // 2
+# a //= 2
+
+# print(a)
+
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+#
+# print()
+#
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+
+# print(not True and True)
+# print(not True and False)
+# print(not False and True)
+# print(not False and False)
+
+# print(not (True and True))
+# print(not (True and False))
+# print(not (False and True))
+# print(not (False and False))
+
+
+# print(True or False or True or False or True)
+
+# print(True and False and True and False and True)
+
+
+# print(False or False and True and True and True)
+
+
+# print(6 > 4 or 4 > 6)
+# print(6 > 4 and 4 > 6)
+
+# print(-8 or 4 or 3 or 2 or 1)
+# print(0 or 4 or 3 or 2 or 1)
+# print(0 or '' or 3 or 2 or 1)
+# print(0 or '' or 0.0 or 2 or 1)
+# print(0 or '' or 0.0 or 0j or 1)
+# print(0 or '' or 0.0 or None or False)
+
+# print(0j and 4 and 0 and 2 and 1)
+
+
+# print(8 > 4)
+# print(8 < 4)
+# print(8 >= 4)
+# print(8 <= 4)
+
+# a = 5
+# b = 2
+#
+# print(a == b)
+# print(a != b)
+
+
+# a = 5
+# b = 5
+
+# print(id(a))
+# print(id(b))
+
+# print(a is b)
+# print(a == b)
+
+# lst1 = [5]
+# lst2 = [5]
+
+# print(id(lst1))
+# print(id(lst2))
+
+# print(lst1 is lst2)
+# print(lst1 == lst2)
+
+
+# a = 5
+# b = 5
+# c = a
+#
+# a += 6
+#
+# print(a)
+# print(b)
+# print(c)
+
+# print(id(a))
+# print(id(b))
+# print(id(c))
+
+# a = "O"
+# name = "otar"
+#
+# print(a in name)
+
+# a = 7
+# b = 245
+#
+# print(str(a) in str(b))
+
+# name1 = input("Please enter name 1: ")
+# name2 = input("Please enter name 2: ")
+# name3 = input("Please enter name 3: ")
+#
+# if name1 == "otar":
+#     print("Hello otar")
+# elif name2 == "levan":
+#     print("Hello levan")
+# elif name3 == "anri":
+#     print("Hello anri")
+# else:
+#     print("Hello stranger")
+#
+# print("End of program")
+
+
+# name1 = input("Please enter name 1: ")
+# name2 = input("Please enter name 2: ")
+#
+# if name1 == "otar":
+#     print("Hello otar")
+# if name2 == "levan":
+#     print("Hello levan")
+# else:
+#     print("Hello stranger")
+#
+# print("End of program")
+
+
+# username = "admin"
+# password = "admin123"
+#
+# user_username = input("Please enter username: ")
+# user_password = input("Please enter password: ")
+#
+# if user_username == username and user_password == password:
+#     print("Login successful")
+# else:
+#     print("Invalid username or password")
+
+
+# username = "admin"
+# password = "admin123"
+# phone_number = "123"
+#
+# user_username = input("Please enter username: ")
+#
+# if user_username == username:
+#     user_password = input("Please enter password: ")
+#
+#     if user_password == password:
+#         user_phone_number = input("Please enter phone number: ")
+#
+#         if user_phone_number == phone_number:
+#             print("Login successful")
+#         else:
+#             print("Invalid phone number")
+#     else:
+#         print("Invalid password")
+# else:
+#     print("Invalid username")
